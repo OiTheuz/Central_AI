3faa95b57b85bfc45b137f785e094a53f0828d71
Oitheuz <matheus.polivalente@gmail.com>
2026-06-22 23:40:34 -0300

    chore: remove script temporario

--- Central_IA/check_merchants.py
+++
@@ -1,7 +0,0 @@
-from app.database import SessionLocal
-from sqlalchemy import text
-
-db = SessionLocal()
-r = db.execute(text("SELECT nome_loja FROM public.merchant"))
-print(r.fetchall())
-db.close()
